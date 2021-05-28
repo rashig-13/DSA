1083becf795f580ceb21829d22fc5cc7f32ba0d8
rashig-13 <gupta.rashi130999@gmail.com>
2021-05-28 17:30:57 +0530

    added a question

---
+++ Backtracking/permutations_lc.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void solve(vector<int>& v,vector<vector<int>> &ans,int ind){
+       if(ind==v.size()){
+           ans.push_back(v);
+           return ;
+       }        
+        for(int i=ind;i<v.size();i++){
+            swap(v[i],v[ind]);
+            solve(v,ans,ind+1);
+            swap(v[i],v[ind]);
+        }
+        return ;
+    }
+    vector<vector<int>> permute(vector<int>& v) {
+        vector<vector<int>> ans;
+        solve(v,ans,0);
+        return ans;
+    }
+};
