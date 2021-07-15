1bc260f535a543912fc16c7b96befa4a60ac1742
rashig-13 <gupta.rashi130999@gmail.com>
2021-07-15 14:55:55 +0530

    added dfs que

---
+++ Graphs/Nested_list_weight_sum_lc.cpp
@@ -0,0 +1,57 @@
+You are given a nested list of integers nestedList. Each element is either an integer or a list whose elements may also be integers or other lists.
+
+The depth of an integer is the number of lists that it is inside of. For example, the nested list [1,[2,2],[[3],2],1] has each integer's value set to its depth.
+
+Return the sum of each integer in nestedList multiplied by its depth.
+
+Input: nestedList = [[1,1],2,[1,1]]
+Output: 10
+Explanation: Four 1's at depth 2, one 2 at depth 1. 1*2 + 1*2 + 2*1 + 1*2 + 1*2 = 10.
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Constructor initializes an empty nested list.
+ *     NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     NestedInteger(int value);
+ *
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     void add(const NestedInteger &ni);
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+class Solution {
+public:
+    void dfs(vector<NestedInteger>& nestedList,int depth,int &ans){
+        for(NestedInteger nested : nestedList ){
+            if(nested.isInteger()){
+                ans+=nested.getInteger() * depth;
+            }
+            else
+            dfs(nested.getList(),depth+1,ans);
+        }
+            return ;
+    }
+    int depthSum(vector<NestedInteger>& nestedList) {
+        int ans=0;
+       dfs(nestedList,1,ans);
+        return ans;
+    }
+};
