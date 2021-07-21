25cf92b1c94c7e0cc55e1886774c7c230aa8f324
rashig-13 <gupta.rashi130999@gmail.com>
2021-07-21 10:15:09 +0530

    updated subsets 2

--- Backtracking/subsets_II_lc.cpp
+++ Backtracking/subsets_II_lc.cpp
@@ -25,3 +25,28 @@ public:
         return ans;
     }
 };
+
+//sol2
+class Solution {
+public:
+   void solve(vector<int>& v,set<vector<int>> &s,vector<int> &tmp,int ind){
+      s.insert(tmp);
+       for(int i=ind;i<v.size();i++){
+           tmp.push_back(v[i]);
+           solve(v,s,tmp,i+1);
+           tmp.pop_back();
+       }
+       
+   }
+    vector<vector<int>> subsetsWithDup(vector<int>& v) {
+        set<vector<int>> s;
+        vector<int> tmp;
+        sort(v.begin(),v.end());
+        solve(v,s,tmp,0);
+        vector<vector<int>> ans;
+        for(auto x : s){
+            ans.push_back(x);
+        }
+        return ans;
+    }
+};
