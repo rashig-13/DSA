a0afe010f74cbb28a026e4bc452b0c9aeffe49db
rashig-13 <gupta.rashi130999@gmail.com>
2021-07-21 12:49:01 +0530

    updated combination sum 3

--- Backtracking/combination_sum3_lc.cpp
+++ Backtracking/combination_sum3_lc.cpp
@@ -1,4 +1,9 @@
-// given n & k , find all subsets of size k which sum to n and subset can have nos. from 1 to 9
+Find all valid combinations of k numbers that sum up to n such that the following conditions are true:
+
+Only numbers 1 through 9 are used.
+Each number is used at most once.
+Return a list of all possible valid combinations.
+ The list must not contain the same combination twice, and the combinations may be returned in any order.
 class Solution {
 public:
     void solve(int k,int n,vector<vector<int>> &ans,vector<int> &tmp,int cur){
