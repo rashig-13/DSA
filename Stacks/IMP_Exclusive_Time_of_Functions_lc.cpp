f09c9017728b8d270c34be255f984ab00863417c
rashig-13 <gupta.rashi130999@gmail.com>
2021-07-22 22:04:38 +0530

    added exclusive time of functions

---
+++ Stacks/IMP_Exclusive_Time_of_Functions_lc.cpp
@@ -0,0 +1,48 @@
+//636
+On a single-threaded CPU, we execute a program containing n functions. Each function has a unique ID between 0 and n-1.
+
+Function calls are stored in a call stack: when a function call starts, its ID is pushed onto the stack, and when a function call ends, its ID is popped off the stack.
+ The function whose ID is at the top of the stack is the current function being executed. Each time a function starts or ends, we write a log with the ID, whether it started or ended, and the timestamp.
+
+You are given a list logs, where logs[i] represents the ith log message formatted as a string "{function_id}:{"start" | "end"}:{timestamp}".
+ For example, "0:start:3" means a function call with function ID 0 started at the beginning of timestamp 3, and "1:end:2" means a function call with function ID 1 ended at the end of timestamp 2. 
+ Note that a function can be called multiple times, possibly recursively.
+
+A functions exclusive time is the sum of execution times for all function calls in the program. 
+For example, if a function is called twice, one call executing for 2 time units and another call executing for 1 time unit, the exclusive time is 2 + 1 = 3.
+
+Return the exclusive time of each function in an array, where the value at the ith index represents the exclusive time for the function with ID i.
+
+class Solution {
+public:
+    struct log {
+        int id;
+        string status;
+        int time;
+    };
+    vector<int> exclusiveTime(int n, vector<string>& logs) {
+        vector<int> ans(n,0);
+        stack<log> st;
+        for(string log: logs){
+            istringstream ss(log);
+            string tmp1,tmp2,tmp3;
+            getline(ss,tmp1,':');
+            getline(ss,tmp2,':');
+            getline(ss,tmp3,':');
+            
+           struct log item={stoi(tmp1),tmp2,stoi(tmp3)};
+            if(item.status=="start"){
+                st.push(item);
+            }
+            else{
+                int req_time=item.time-st.top().time+1;
+                ans[item.id]+=req_time;
+                st.pop();
+                if(!st.empty()){
+                    ans[st.top().id]-=req_time;
+                }
+            }
+        }
+        return ans;
+    }
+};
